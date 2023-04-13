959634dda478e65edec8d9342e1754ad1026c54b
Hong <joeleehong@gmail.com>
2023-04-13 12:58:13 -0700

    fixed reducer

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,17 +1,20 @@
 //import { FETCH_MOVIES } from "./constant"
-import {RECEIVE_MOVIES} from "./Actions"
+import { RECEIVE_MOVIES } from "./Actions";
 
 const initialState = {
-    movies : [],
-    likedMovies : [],
-    blockMovies : []
-}
+  movies: [],
+  likedMovies: [],
+  blockMovies: [],
+};
 
-const Reducer = (state=initialState, action)=>{
-    switch(action.type) {
-        case RECEIVE_MOVIES:
-            return [...state.movies, action.payload]
-            /*
+const Reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case RECEIVE_MOVIES:
+      return {
+        ...state,
+        movies: action.payload,
+      };
+    /*
         case 'likedList/like':
             return {}
         case 'likedList/dislike':
@@ -20,10 +23,9 @@ const Reducer = (state=initialState, action)=>{
             return {}
         case 'blockList/unblock':
             return {} */
-
-        default:
-            return state
-    }
-}
+    default:
+      return state;
+  }
+};
 
 export default Reducer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,14 @@
-import {useDispatch, useSelector} from 'react-redux'
-import React, { useEffect } from 'react';
-import { fetchMovies } from '../Actions'
-// Prev, Next Button, Sort Button, 
-const Home = function() {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        fetchMovies(10);
-      }, []);
-    const movies = useSelector(state=>state.movies);
-    console.log(movies);
-      return (
-        <div>
-          
-        </div>
-      );
-}
+import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { fetchMovies } from "../Actions";
+// Prev, Next Button, Sort Button,
+const Home = function () {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMovies(5));
+  }, []);
+  const movies = useSelector((state) => state.movies);
+  console.log(movies);
+  return <div></div>;
+};
 export default Home;
